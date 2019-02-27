3163cda7ea497313e5a859d50b8c376244b410df
github username <github email address>
2019-02-27 09:46:59 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,53 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    plate: [],
+    emptyPlate: [],
+    money: 999999999999
+  }
+
+  componentDidMount = () => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushi => {
+      this.setState({
+        sushi: sushi,
+        plate: sushi
+      })
+    })
+  }
+
+  ateSushi = receivedSushi => {
+    let bill = 0;
+    let finishedPlate = [...this.state.emptyPlate]
+    let ateTheSushi = this.state.plate.map(sushi => {
+      if (sushi == receivedSushi && sushi.img_url) {
+        sushi.img_url = ""
+        console.log(`Ate ${sushi.name}!`)
+        bill = sushi.price
+        finishedPlate.push(sushi.name)
+      }
+      return sushi
+    })
+
+    this.setState({
+      plate: ateTheSushi,
+      money: this.state.money - bill,
+      emptyPlate: finishedPlate
+    })
+  }
+
+  
+
+
   render() {
+    console.log(this.state.emptyPlate)
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushi={this.state.sushi} ateSushi={this.ateSushi} />
+        <Table money={this.state.money} ateSushi={this.ateSushi} emptyPlate={this.state.emptyPlate} />
       </div>
     );
   }
